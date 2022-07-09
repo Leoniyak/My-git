4cce8e14a49f904bb16b522cb75cd617ad5f1df4
Leoniyak <iyakareleon@gmail.com>
2022-07-09 14:44:46 +0000

    My first commit

---
+++ my_age.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#Written by Leon
+#This script give my an opinion about my youth
+
+echo "Hello, what is your name ?"
+read PERSON
+echo "Hello, $PERSON"
+echo "What is your age $PERSON ?"
+read AGE
+echo " You are too old $PERSON"
